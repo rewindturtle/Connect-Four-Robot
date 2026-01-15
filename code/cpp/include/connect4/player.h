08a267a5f3f7adca87411b8fb9570669d064b757
rewindturtle <nthnw30@gmail.com>
2026-01-14 19:07:21 -0700

    Fixed negamax algorithm in Player class

--- code/cpp/include/connect4/player.h
+++ code/cpp/include/connect4/player.h
@@ -7,31 +7,17 @@
 
 namespace connect4 {
     class Player {
-        struct BoardInfo {
-            enum Flag : uint8_t {
-                NOT_SET = 0,
-                EXACT = 1,
-                LOWERBOUND = 2,
-                UPPERBOUND = 3
-            };
-
-            int8_t score = 0;
-            Flag flag = NOT_SET;
-
-            BoardInfo() : score(0), flag(NOT_SET) {}
-        };
-
-        using BoardMap = std::unordered_map<Board, BoardInfo>;
+        using BoardMap = std::unordered_map<Board, int8_t, BoardHash>;
+        using ScoreArray = std::array<int8_t, 7>;
 
         private:
-            BoardMap _memo;
             Board _board;
-            uint8_t _currentTurn = 0;
             uint8_t _maxDepth = 7;
+            uint8_t _currentTurn = 0;
 
-            int8_t _negamaxPlayer(const Board& board, uint8_t depth, int8_t alpha, int8_t beta);
-            int8_t _negamaxOpponent(const Board& board, uint8_t depth, int8_t alpha, int8_t beta);
-            std::array<int8_t, 7> _getMoveScores(const Board& board);
+            int8_t _negamaxPlayer(const Board& board, uint8_t depth, int8_t alpha, int8_t beta, BoardMap& memo);
+            int8_t _negamaxOpponent(const Board& board, uint8_t depth, int8_t alpha, int8_t beta, BoardMap& memo);
+            void _getScores(const Board& board, ScoreArray& scores);
         public:
             Player() : _board() {}
 
--- code/cpp/src/connect4/player.cpp
+++ code/cpp/src/connect4/player.cpp
@@ -4,7 +4,20 @@ using namespace connect4;
 
 
 // Refer to https://en.wikipedia.org/wiki/Negamax#Negamax_with_alpha_beta_pruning_and_transposition_tables
-int8_t Player::_negamaxPlayer(const Board& board, uint8_t depth, int8_t alpha, int8_t beta) {
+int8_t Player::_negamaxPlayer(const Board& board, uint8_t depth, int8_t alpha, int8_t beta, BoardMap& memo) {
+    auto it = memo.find(board);
+    if (it != memo.end()) {
+        return it->second;
+    }
+
+    if (board.opponentWins()) {
+        memo[board] = -depth;
+        return -depth;
+    } else if (depth == 0 || board.isDraw()) {
+        memo[board] = 0;
+        return 0;
+    }
+
     int8_t originalAlpha = alpha;
 
     int8_t score = -42;
@@ -16,194 +29,66 @@ int8_t Player::_negamaxPlayer(const Board& board, uint8_t depth, int8_t alpha, i
         Board newBoard = board;
         newBoard.placePlayer(col);
 
-        auto it = _memo.find(newBoard);
-        if (it == _memo.end()) {
-            it = _memo.emplace(newBoard, BoardInfo{}).first;
-        } else {
-            // If two states are identical, they have the same amount of turns played
-            // So we don't need to check depth here
-            switch (it->second.flag) {
-                case BoardInfo::EXACT:
-                    return it->second.score;
-                case BoardInfo::LOWERBOUND:
-                    if (it->second.score >= beta) {
-                        return it->second.score;
-                    }
-                    break;
-                case BoardInfo::UPPERBOUND:
-                    if (it->second.score <= originalAlpha) {
-                        return it->second.score;
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        if (newBoard.playerWins()) {
-            score = 42 - static_cast<int8_t>(_currentTurn + depth);
-            it->second.score = score;
-            it->second.flag = BoardInfo::EXACT;
-            return score;
-        } else if (board.isDraw()) {
-            it->second.score = 0;
-            it->second.flag = BoardInfo::EXACT;
-            return 0;
-        } else if (depth == _maxDepth) {
-            continue;
-        }
-
-        
-    }
-
-    auto it = _memo.find(board);
-    if (it == _memo.end()) {
-        it = _memo.emplace(board, BoardInfo{}).first;
-    } else {
-        // If two states are identical, they have the same amount of turns played
-        // So we don't need to check depth here
-        if (it->second.flag == BoardInfo::EXACT) {
-            return it->second.score;
-        } else if (it->second.flag == BoardInfo::LOWERBOUND && it->second.score >= beta) {
-            return it->second.score;
-        } else if (it->second.flag == BoardInfo::UPPERBOUND && it->second.score <= originalAlpha) {
-            return it->second.score;
-        }
-    }
-
-    int8_t score = -42;
-    for (uint8_t col = 0; col < 7; ++col) {
-        if (board.isColumnFull(col)) {
-            continue;
-        }
-
-        Board newBoard = board;
-        newBoard.placePlayer(col);
-
-        if (newBoard.playerWins()) {
-            score = 42 - static_cast<int8_t>(_currentTurn + depth);
-            it->second.score = score;
-            it->second.flag = BoardInfo::EXACT;
-            return score;
-        } else if (board.isDraw()) {
-            it->second.score = 0;
-            it->second.flag = BoardInfo::EXACT;
-        } else if (depth == _maxDepth) {
-            continue;
-        }
-
-        score = std::max<int8_t>(score, -_negamaxOpponent(newBoard, depth + 1, -alpha, -beta));
+        score = std::max<int8_t>(score, -_negamaxOpponent(newBoard, depth - 1, -beta, -alpha, memo));
         alpha = std::max<int8_t>(alpha, score);
         if (alpha >= beta) {
             break;
         } 
     }
 
-    it->second.score = score;
-    if (score <= originalAlpha) {
-        it->second.flag = BoardInfo::UPPERBOUND;
-    } else if (score >= beta) {
-        it->second.flag = BoardInfo::LOWERBOUND;
-    } else {
-        it->second.flag = BoardInfo::EXACT;
-    }
-
+    memo[board] = score;
     return score;
 }
 
 
-int8_t Player::_negamaxOpponent(const Board& board, uint8_t depth, int8_t alpha, int8_t beta) {
+int8_t Player::_negamaxOpponent(const Board& board, uint8_t depth, int8_t alpha, int8_t beta, BoardMap& memo) {
+    auto it = memo.find(board);
+    if (it != memo.end()) {
+        return it->second;
+    }
+
     if (board.playerWins()) {
-        return static_cast<int8_t>(_currentTurn + depth) - 42;
-    } else if (depth == _maxDepth || board.isDraw()) {
+        memo[board] = -depth;
+        return -depth;
+    } else if (depth == 0 || board.isDraw()) {
+        memo[board] = 0;
         return 0;
     }
 
     int8_t originalAlpha = alpha;
 
-    auto it = memo.find(board);
-    if (it == memo.end()) {
-        int8_t score = -42;
-        for (uint8_t col = 0; col < 7; ++col) {
-            if (board.isColumnFull(col)) {
-                continue;
-            }
-
-            Board newBoard = board;
-            newBoard.placeOpponent(col);
-            score = std::max<int8_t>(score, -_negamaxPlayer(newBoard, depth + 1, -alpha, -beta));
-            alpha = std::max<int8_t>(alpha, score);
-            if (alpha >= beta) {
-                break;
-            }
+    int8_t score = -42;
+    for (uint8_t col = 0; col < 7; ++col) {
+        if (board.isColumnFull(col)) {
+            continue;
         }
 
-        BoardInfo& info = memo[board];
-        info.score = score;
-        info.depth = depth;
-        if (score <= originalAlpha) {
-            info.flag = BoardInfo::UPPERBOUND;
-        } else if (score >= beta) {
-            info.flag = BoardInfo::LOWERBOUND;
-        } else {
-            info.flag = BoardInfo::EXACT;
-        }
-        return score;
-    } else {
-        if (it->second.depth <= depth) {
-            if (it->second.flag == BoardInfo::EXACT) {
-                return it->second.score;
-            } else if (it->second.flag == BoardInfo::LOWERBOUND && it->second.score >= beta) {
-                return it->second.score;
-            } else if (it->second.flag == BoardInfo::UPPERBOUND && it->second.score <= originalAlpha) {
-                return it->second.score;
-            }
-        }
+        Board newBoard = board;
+        newBoard.placeOpponent(col);
 
-        int8_t score = -42;
-        for (uint8_t col = 0; col < 7; ++col) {
-            if (board.isColumnFull(col)) {
-                continue;
-            }
-
-            Board newBoard = board;
-            newBoard.placeOpponent(col);
-            score = std::max<int8_t>(score, -_negamaxPlayer(newBoard, depth + 1, -alpha, -beta));
-            alpha = std::max<int8_t>(alpha, score);
-            if (alpha >= beta) {
-                break;
-            }
-        }
-
-        it->second.score = score;
-        it->second.depth = depth;
-        if (score <= originalAlpha) {
-            it->second.flag = BoardInfo::UPPERBOUND;
-        } else if (score >= beta) {
-            it->second.flag = BoardInfo::LOWERBOUND;
-        } else {
-            it->second.flag = BoardInfo::EXACT;
-        }
-
-        return score;
+        score = std::max<int8_t>(score, -_negamaxPlayer(newBoard, depth - 1, -beta, -alpha, memo));
+        alpha = std::max<int8_t>(alpha, score);
+        if (alpha >= beta) {
+            break;
+        } 
     }
+
+    memo[board] = score;
+    return score;
 }
 
 
-std::array<int8_t, 7> Player::_getMoveScores(const Board& board) {
-    std::array<int8_t, 7> moveScores = {};
+void Player::_getScores(const Board& board, ScoreArray& scores) {
     BoardMap memo;
 
     for (uint8_t col = 0; col < 7; ++col) {
         if (board.isColumnFull(col)) {
-            moveScores[col] = -42;
+            scores[col] = -42;
             continue;
         }
 
         Board newBoard = board;
         newBoard.placePlayer(col);
-        moveScores[col] = -_negamaxOpponent(newBoard, 1, -42, 42, memo);
+        scores[col] = -_negamaxOpponent(newBoard, _maxDepth - 1, -42, 42, memo);
     }
-
-    return moveScores;
 }
